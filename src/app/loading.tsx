5f5cf96f5074fef524f5f8368510213c494d7fcd
yoonjin <skyj217@naver.com>
2024-09-06 20:57:07 +0900

    feat: add pages

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import NavButton from '../components/NavButton';  // NavButton import
 
 const Loading: React.FC = () => {
     return (
         <div className="flex items-center justify-center h-screen flex-col">
             <h2 className="text-4xl font-bold">분석중 ... 🏃‍♀️</h2>
             <p className="text-lg mt-4">잠시만 기다려주세요!</p>
-            
-            {/* NavButton 컴포넌트 사용 */}
-            <NavButton direction="right" link="/recommendPlaces" />
         </div>
     );
 }
